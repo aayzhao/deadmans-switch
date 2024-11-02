a57e2a863198df85169389654d163af5074a68cd
Aaron Zhao <aaron.y.zhao1@gmail.com>
2024-11-02 18:16:14 -0400

    initial commit for auth impl

--- frontend/deadmans-switch-app/src/app/services/authentication/authentication.service.ts
+++ frontend/deadmans-switch-app/src/app/services/authentication/authentication.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
-    
+  private apiUrl = 'mongodb+srv://admin:hfuyfrGd5rOT95Qg@cluster0.9shfq.mongodb.net/deadmans-switch?retryWrites=true&w=majority&appName=Cluster0';
+
+  constructor(private http: HttpClient) {}
+
+  register(userData: { username: string; password: string; email: string }): Observable<any> {
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    return this.http.post<any>(this.apiUrl, userData, { headers });
+  }
 }
