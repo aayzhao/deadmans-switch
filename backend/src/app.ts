f931c69173e4567faa7734795702ba7064896aa3
peasantbird <bertrand.wong.xh@gmail.com>
2024-11-02 14:44:16 -0400

    Add skeleton

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,3 +1,11 @@
-import express, { Express } from 'express';
+import dotenv from 'dotenv'
+import express, { Express } from 'express'
 
-const app: Express = express();
+dotenv.config()
+
+const app: Express = express()
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
