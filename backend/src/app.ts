30b06529f12b508ecd461bfe05001d3e9ec73893
peasantbird <bertrand.wong.xh@gmail.com>
2024-11-02 16:00:39 -0400

    Add database connection

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,6 +5,9 @@ dotenv.config()
 
 const app: Express = express()
 const port = process.env.PORT || 3000
+const dbConnect = require('./db/dbConnect')
+
+dbConnect()
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
