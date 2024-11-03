28d3209f2182b2c2c8332ad477ce6e9150d2699a
Justin Guo <justing1618@gmail.com>
2024-11-02 20:03:32 -0400

    auth successful

--- frontend/deadmans-switch-app/src/app/services/authentication/authentication.service.ts
+++ frontend/deadmans-switch-app/src/app/services/authentication/authentication.service.ts
@@ -6,22 +6,22 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class AuthenticationService {
-  private apiUrl = 'mongodb+srv://admin:hfuyfrGd5rOT95Qg@cluster0.9shfq.mongodb.net/deadmans-switch?retryWrites=true&w=majority&appName=Cluster0';
+  private apiUrl = 'http://localhost:3000';
 
   constructor(private http: HttpClient) {}
 
   register(userData: { username: string; password: string; email: string }): Observable<any> {
     const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
-    return this.http.post<any>(this.apiUrl, userData, { headers });
+    return this.http.post<any>(this.apiUrl+'/register', userData, { headers });
   }
 
   login(userData: { username: string; password: string}): Observable<any> {
     const headers = new HttpHeaders({ 'Content_type': 'application/json' });
-    return this.http.post<any>(this.apiUrl, userData, {headers})
+    return this.http.post<any>(this.apiUrl+'/login', userData, {headers})
   }
 
   logout(): Observable<any> {
     const headers = new HttpHeaders({ 'Content_type': 'application/json' });
-    return this.http.post<any>(this.apiUrl, {headers})
+    return this.http.post<any>(this.apiUrl+'/logout', {headers})
   }
 }
