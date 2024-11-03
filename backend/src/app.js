c734936c92f3f647ca002a8b92bd10277064e418
peasantbird <bertrand.wong.xh@gmail.com>
2024-11-03 00:09:27 -0400

    Add expiry check cron job

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,7 @@ import express from "express";
 import dbConnect from "../db/dbConnect.js";
 import { router } from "./routes/routes.js";
 import errorHandler from "./middlewares/error.middleware.js";
+import "./controllers/expiry/expiry.js"; // Importing this file will run the cron job
 
 dotenv.config();
 
